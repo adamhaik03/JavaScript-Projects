d8da8a09dc775b41d44959f0459444eabbabaa4b
adamhaik03 <107291787+adamhaik03@users.noreply.github.com>
2022-11-08 05:25:32 +0000

    Update main2.js

--- Basic JavaScript Projects/Project2_functions/JS/main2.js
+++ Basic JavaScript Projects/Project2_functions/JS/main2.js
@@ -1,5 +1,5 @@
-function myFunction2 () {
-    var klej = "Wish you merry";
-    klej += " christmas.";
-    document.getElementById("guzik").innerHTML = klej;
+function myFunction2 () { //Defining a function and naming it
+    var klej = "Wish you merry"; //Defining a variable and giving it a string value
+    klej += " christmas."; //Using += operator to concatenate strings
+    document.getElementById("guzik").innerHTML = klej; //Putting the value of result into HTML element with "guzik" id
 }
