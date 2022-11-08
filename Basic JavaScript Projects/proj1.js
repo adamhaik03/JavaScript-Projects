091bbfd5910c42ff7c4f91c2c4c657e1c810e6a2
adamhaik03 <107291787+adamhaik03@users.noreply.github.com>
2022-11-08 03:43:35 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/proj1.js
@@ -0,0 +1,7 @@
+var A = "Cats"; //Defining a variable and giving it a string value
+var B = "Dogs"; //Defining a variable and giving it a string value
+var A = A.fontcolor("blue"); //Using a fontcolor on A variable
+var B = B.fontcolor("red"); //Using a fontcolor on B variable
+
+document.write("My favourite animals are " + A + " and " + B + ". "); //Concatenating two variables together
+document.write(18+3); //Writing an expression
